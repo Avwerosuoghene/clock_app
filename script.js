0d81d83e38d9e3c45749497c45fa0d51da8a1d0e
Avwerosuoghene <kesuion1@gmail.com>
2021-12-27 15:46:05 +0100

    adds all functionalities

--- script.js
+++ script.js
@@ -2,7 +2,16 @@ const detailsButton = document.querySelector(".more-details");
 const clockConatiner = document.querySelector(".clock-container");
 const dayIcons = ["assets/desktop/icon-sun.svg", "assets/desktop/icon-moon.svg"];
 const quoteURL = "https://programming-quotes-api.herokuapp.com/Quotes/random";
+const countriesURL = "https://api.timezonedb.com/v2.1/list-time-zone?key=D7K6G69S5O7E&format=json";
+const timeSetURL = "http://worldtimeapi.org/api/timezone/";
+const countriesList = [];
+const countryObjectsArray = [];
 const refreshQuote = document.getElementById("refresh-icon");
+const countrySelect = document.querySelector(".country-select");
+const searchCountry = document.querySelector(".search-country");
+const countriesContainer = document.querySelector(".countries-container");
+let count = 0;
+
 
 detailsButton.addEventListener ("click", (e) => {
     
@@ -25,10 +34,36 @@ detailsButton.addEventListener ("click", (e) => {
     }
 });
 
-function setTime () {
-    const date = new Date();
-    const hour = date.getHours();
-    let minute = date.getMinutes();
+refreshQuote.addEventListener ("click", () => {
+    getQuote();
+})
+
+async function getQuote () {
+    const quote = document.getElementById("quote");
+    const author = document.getElementById("author");
+
+    try {
+        const { data } = await axios(quoteURL);
+        quote.innerText = data.en;
+        author.innerText = data.author;
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+countrySelect.addEventListener("click", () => {
+    countriesContainer.classList.toggle("full");
+
+});
+
+searchCountry.addEventListener("input", (e) => searchConfirm(e.target.value));
+
+function setParams (data, countryname) {
+    
+    const regex = /([\d-]*)T(\d{2}):(\d{2}):(\d{2})/;
+    const dateArr = regex.exec(data.datetime);
+    const hour = dateArr[2];
+    const minute = dateArr[3]
 
     if (String (minute).length <= 1) {
         minute = `0${minute}`
@@ -37,17 +72,46 @@ function setTime () {
     
     const timeZone = document.querySelector(".current-time");
     timeZone.innerText = `${hour}:${minute}`
+
+    const zoneAbbrev = document.querySelector(".zone_abbv");
+    zoneAbbrev.innerHTML = data.abbreviation;
+
+    const location = document.querySelector(".location");
+    location.innerHTML = `IN ${countryname}`
+
+    const timeZoneText = document.querySelector("#time-zone");
+    timeZoneText.innerText = data.timezone;
+
+    const DOY = document.getElementById("doy");
+    const DOW = document.getElementById("dow");
+    const WeekNo = document.getElementById("week-no");
+
+    DOY.innerText = data.day_of_year;
+    DOW.innerText = data.day_of_week;
+    WeekNo.innerText = data.week_number;
     
         if (hour >= 5 && hour <= 11){
             setStatus("Good morning", dayIcons[0]);
             setBackground("day");
         } else if (hour >= 12 && hour <= 17) {
             setStatus("Good afternoon");
+            setBackground("day");
         } else {
             setStatus("Good evening");
             setBackground("night");
         }
     
+}
+
+
+function searchConfirm (searchTerm) {
+    countriesList.forEach((country) => {
+        if(country.innerHTML.toLowerCase().includes(searchTerm.toLowerCase())){
+            country.classList.remove("hide");
+        } else {
+            country.classList.add ("hide");
+        }
+    })
 }
 
 function setStatus(greet) {
@@ -72,24 +136,73 @@ function setBackground (day) {
     }
 }
 
-refreshQuote.addEventListener ("click", () => {
-    getQuote();
-})
-
-async function getQuote () {
-    const quote = document.getElementById("quote");
-    const author = document.getElementById("author");
+async function getCountries() {
+    const countries = document.querySelector(".countries");
 
     try {
-        const { data } = await axios(quoteURL);
-        quote.innerText = data.en;
-        author.innerText = data.author;
+        const {data} = await axios(countriesURL);
+        countries.innerHTML = ""
+        const zones = data.zones;
+        zones.forEach(zone => {
+            const country = document.createElement("li");
+            country.classList.add("country");
+            country.innerText = zone.countryName;
+            const countryObj = {
+                name:zone.countryName,
+                timezone:zone.zoneName
+            }
+            countryObjectsArray.push(countryObj);
+            countriesList.push(country);
+            countries.appendChild(country);
+        })
+        pickCountry()
     } catch (err) {
-        console.log(err)
-    }
+        console.log(err);
+    }   
 }
+
+function pickCountry () {
+    countriesList.forEach(country => {
+        if (country.innerText == "Nigeria" && count == 0) {
+            console.log(country.innerText)
+            count = 1;
+            countryObjectsArray.forEach((countObj) => {
+            if(country.innerText === countObj.name){
+                setZone(countObj.timezone, country.innerText)
+            } else {
+                
+            }
+        })
+            
+        }
+
+        country.addEventListener("click", (e) => {
+            countryObjectsArray.forEach((countObj) => {
+                if(e.target.innerHTML === countObj.name){
+                    countriesContainer.classList.toggle("full");
+                    setZone(countObj.timezone, e.target.innerHTML)
+                } else {
+                    
+                }
+            })
+        })
+    }) 
+    
+}
+
+async function setZone (selectedZone, countryname) {
+        try{
+            const {data} = await axios.get(timeSetURL + selectedZone);
+            setParams(data, countryname)
+        } catch (err) {
+            console.log(err)
+        }
+}
+
+
+getCountries();
 
 getQuote();
 
 
-setTime();
+// setTime();
--- test.js
+++ test.js
@@ -1,3 +1,19 @@
-const val = 22
+const timestamp = "2021-12-26T20:55:30.744074+00:00"
 
-console.log(String(val).length)
+
+const regex = /([\d-]*)T(\d{2}):(\d{2}):(\d{2})/
+const matches = regex.exec(timestamp);
+
+
+let matchArray ;
+
+console.log(matches[3])
+
+// if (timestamp.match(regex)){
+//     console.log(x
+// }
+
+
+
+
+
